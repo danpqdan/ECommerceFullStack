df9e2f5acb9ddbabe6837f8d7c636b51b27f6e23
Daniel J. Santos <danieltisantos@gmail.com>
2024-11-11 14:06:03 -0300

    atualizado projeto para build

--- java/apiecomerce/src/main/java/com/apiecommerce/apiecomerce/ApiecomerceApplication.java
+++ java/apiecomerce/src/main/java/com/apiecommerce/apiecomerce/ApiecomerceApplication.java
@@ -2,16 +2,13 @@ package com.apiecommerce.apiecomerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin("*")
 public class ApiecomerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiecomerceApplication.class, args);
 
-		
 	}
 
 }
